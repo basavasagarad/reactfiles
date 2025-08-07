47844ea3861668bcce9c5a7b032271b21526414a
basavasagarad <156989346+basavasagarad@users.noreply.github.com>
2025-08-08 00:25:53 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+  
+
+  return (
+    <>
+      <Chai/>
+      <h2>hey its working</h2>
+    </>
+     
+  )
+}
+
+export default App
---
+++ Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1>This is my first react page</h1>
+    )
+}
+export default Chai
